eba6be7ba2f7723c8ebbf9ddddbcb7c5bb3dea53
Marcos Bujosa <mbujosab@ucm.es>
2025-07-24 22:15:59 +0200

    Cambios sugeridos por ChatGPT

--- makefile
+++ makefile
@@ -1,16 +1,18 @@
-# Directorios
 LECCIONES_SRC = ./org-lessons
 LECCIONES = ./lecciones
 DOCS = ./docs
 CUADERNOS = $(DOCS)/CuadernosElectronicos
 TRANSPARENCIAS = $(DOCS)/Transparencias
 
-# Patrón para los archivos fuente
 SRC_FILES = $(LECCIONES_SRC)/Lecc*.org
 
-# Regla principal
+.PHONY: all clean cleanAll directorios series_formales calendario notebooksYslides
+
 all: notebooksYslides calendario
-	emacs -q -l ~/Software/scimax/init.el -batch -nw -l publica.el
+	emacs --batch \
+	  --load ~/.emacs.d/no-tlmgr.el \
+	  --load ~/Software/scimax/init.el \
+	  -l publica.el
 
 calendario: $(DOCS)/Calendario-Econometria-Aplicada.pdf
 
@@ -20,24 +22,34 @@ $(DOCS)/Calendario-Econometria-Aplicada.pdf:
 
 notebooksYslides: $(patsubst $(LECCIONES_SRC)/%.org,$(CUADERNOS)/%.ipynb,$(wildcard $(SRC_FILES)))
 
-# Regla para generar notebooks y slides
 $(CUADERNOS)/%.ipynb $(TRANSPARENCIAS)/%.slides.html: $(LECCIONES_SRC)/%.org
-	make directorios 
+	make directorios
 	make series_formales
 	cp -a $(LECCIONES_SRC)/*.bib $(LECCIONES)
 	cp -a $< $(LECCIONES)
-	emacs -q -l ~/Software/scimax/init.el $(LECCIONES)/$(@F:.ipynb=.org) --batch -f org-babel-execute-buffer --kill
+	emacs --batch \
+	  --load ~/.emacs.d/no-tlmgr.el \
+	  --load ~/Software/scimax/init.el \
+	  --eval "(setq org-confirm-babel-evaluate nil)" \
+	  --eval "(require 'ox-ipynb)" \
+	  --eval "(require 'ob-jupyter)" \
+	  $(LECCIONES)/$(@F:.ipynb=.org) \
+	  -f org-babel-execute-buffer --kill
 	mv $(LECCIONES)/$(@F:.ipynb=.ipynb) $(CUADERNOS)
 	mv $(LECCIONES)/$(@F:.ipynb=.slides.html) $(TRANSPARENCIAS)
 
 series_formales: $(LECCIONES_SRC)/src/implementacion_series_formales.org
-	make directorios 
+	make directorios
 	cp $< $(LECCIONES)/src/
 	emacs --batch $(LECCIONES)/src/implementacion_series_formales.org -l org -f org-babel-tangle
-	ln -s -r $(LECCIONES)/src/implementacion_series_formales.py $(LECCIONES)/
+	#ln -s -r $(LECCIONES)/src/implementacion_series_formales.py $(LECCIONES)/
 	cp -a $(LECCIONES)/src/implementacion_series_formales.py $(CUADERNOS)/src/
-	ln -s -r $(CUADERNOS)/src/implementacion_series_formales.py $(CUADERNOS)/
-	emacs -q -l ~/Software/scimax/init.el -batch -nw --eval "(require 'ox-ipynb)" --eval "(ox-ipynb-export-org-file-to-ipynb-file \"lecciones/src/implementacion_series_formales.org\")"
+	#ln -s -r $(CUADERNOS)/src/implementacion_series_formales.py $(CUADERNOS)/
+	emacs --batch \
+	  --load ~/.emacs.d/no-tlmgr.el \
+	  --load ~/Software/scimax/init.el \
+	  --eval "(require 'ox-ipynb)" \
+	  --eval "(ox-ipynb-export-org-file-to-ipynb-file \"lecciones/src/implementacion_series_formales.org\")"
 	jupyter nbconvert --execute --inplace $(LECCIONES)/src/implementacion_series_formales.ipynb
 	jupyter nbconvert --config mycfg-GitHubPages.py --to slides --reveal-prefix "https://unpkg.com/reveal.js@5.2.1" --execute $(LECCIONES)/src/implementacion_series_formales.ipynb
 	jupyter nbconvert --execute --to html $(LECCIONES)/src/implementacion_series_formales.ipynb
@@ -46,15 +58,15 @@ series_formales: $(LECCIONES_SRC)/src/implementacion_series_formales.org
 	mv $(LECCIONES)/src/implementacion_series_formales.html $(DOCS)
 	touch $@
 
-directorios: 
+directorios:
 	mkdir -v -p $(LECCIONES)/src
 	mkdir -v -p $(LECCIONES)/img
 	mkdir -v -p $(DOCS)/img
 	mkdir -v -p $(DOCS)/pdfs
 	mkdir -v -p $(TRANSPARENCIAS)
 	mkdir -v -p $(CUADERNOS)/src
-	ln -s -r $(DOCS)/img/ $(TRANSPARENCIAS)/
-	ln -s -r $(DOCS)/img/ $(CUADERNOS)/
+	ln -s -r $(DOCS)/img/ $(TRANSPARENCIAS)/ || true
+	ln -s -r $(DOCS)/img/ $(CUADERNOS)/ || true
 	touch directorios
 
 clean:
--- publica.el
+++ publica.el
@@ -1,42 +1,85 @@
+;;; publica.el --- Publicación HTML y PDF para el repositorio Scimax-ELN
+
 (require 'ox-publish)
 
+(setq org-confirm-babel-evaluate nil
+      org-export-with-broken-links t
+      org-html-validation-link nil
+      org-html-head-include-scripts nil
+      org-html-head-include-default-style nil)
+
 (defun my-org-latex-publish-to-pdf (plist filename pub-dir)
-  "Publica un archivo Org como PDF después de ejecutar los bloques de código."
+  "Publica un archivo Org como PDF tras ejecutar bloques de código."
+  (message "Exportando a PDF: %s" filename)
   (with-current-buffer (find-file-noselect filename)
+    (org-babel-execute-buffer)
     (org-latex-publish-to-pdf plist filename pub-dir)))
 
 (let* ((base-directory "./lecciones/")
-       (public-directory "./docs/")
-       (org-export-with-broken-links t)
-       (org-publish-project-alist `(("html"
-				     :base-directory ,base-directory
-				     :base-extension "org"
-                                     :publishing-directory ,public-directory
-				     :exclude "src\\|docs\\|Calendario"
-				     :recursive t
-				     :publishing-function org-html-publish-to-html
-				     :auto-preamble t
-				     :auto-sitemap nil)
-				    
-				    ("pdf"
-				     :base-directory ,base-directory
-				     :base-extension "org"
-				     :publishing-directory ,(concat public-directory "pdfs")
- 				     :exclude "src\\|docs\\|Calendario"
-				     :recursive t
-				     :publishing-function my-org-latex-publish-to-pdf
-				     :auto-preamble t
-				     :auto-sitemap nil)
-				    
-				    ("static-html"
-				     :base-directory ,base-directory
-				     :base-extension "css\\|js\\|png\\|jpg\\|gif\\|pdf\\|dat\\|mov\\|txt\\|svg\\|aiff\\|csv\\|gdt\\|inp\\|ipynb"
-				     :publishing-directory ,public-directory
-				     :exclude "src\\|docs\\|Calendario"
-				     :recursive t
-				     :publishing-function org-publish-attachment)
+       (public-directory "./docs/"))
+  (setq org-publish-project-alist
+        `(("html"
+           :base-directory ,base-directory
+           :base-extension "org"
+           :publishing-directory ,public-directory
+           :exclude "src\\|docs\\|Calendario"
+           :recursive t
+           :publishing-function org-html-publish-to-html
+           :auto-preamble t)
 
-				    ;; ... all the components ...
-				    ("scimax-eln" :components ("html" "static-html" "pdf")))))
+          ("pdf"
+           :base-directory ,base-directory
+           :base-extension "org"
+           :publishing-directory ,(concat public-directory "pdfs")
+           :exclude "src\\|docs\\|Calendario"
+           :recursive t
+           :publishing-function my-org-latex-publish-to-pdf
+           :auto-preamble t)
 
-  (org-publish "scimax-eln" t))
+          ("static-html"
+           :base-directory ,base-directory
+           :base-extension "css\\|js\\|png\\|jpg\\|gif\\|pdf\\|dat\\|mov\\|txt\\|svg\\|aiff\\|csv\\|gdt\\|inp\\|ipynb"
+           :publishing-directory ,public-directory
+           :exclude "src\\|docs\\|Calendario"
+           :recursive t
+           :publishing-function org-publish-attachment)
+
+          ("scimax-eln" :components ("html" "static-html" "pdf"))))
+
+  (message "🟢 Iniciando publicación de lecciones (scimax-eln)...")
+  (org-publish "scimax-eln" t)
+  (message "✅ Publicación de lecciones completada."))
+
+(let* ((base-directory "./"))
+  (setq org-publish-project-alist
+        (append org-publish-project-alist
+                `(("index"
+                   :base-directory ,base-directory
+                   :base-extension "org"
+                   :publishing-directory ,(concat base-directory "docs")
+                   :exclude "docs"
+                   :recursive nil
+                   :publishing-function org-html-publish-to-html
+                   :auto-preamble t
+                   :auto-sitemap t
+                   :with-author nil
+                   :with-creator nil
+                   :with-toc t
+                   :section-numbers nil)
+
+                  ("static-index"
+                   :base-directory ,base-directory
+                   :base-extension "css\\|js\\|png\\|jpg\\|gif\\|pdf\\|dat\\|mov\\|txt\\|svg\\|aiff"
+                   :publishing-directory ,(concat base-directory "docs")
+                   :exclude "docs"
+                   :recursive nil
+                   :publishing-function org-publish-attachment)
+
+                  ("web-repositorio" :components ("index" "static-index")))))
+
+  (message "🟢 Iniciando publicación del índice (web-repositorio)...")
+  (org-publish "web-repositorio" t)
+  (message "✅ Publicación del índice completada."))
+
+(with-temp-file "logs/publica.log"
+  (insert (format "Publicado el %s\n" (current-time-string))))
